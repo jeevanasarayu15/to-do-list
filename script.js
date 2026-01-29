9bdba7deb7312b930e98c4a1cba18290832e4418
jeevanasarayu15 <jeevanakonanki@gmail.com>
2026-01-29 18:48:28 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let inputbox = document.getElementById("inputbox");
+let listcontainer = document.getElementById("list-container");
+
+function addtask() {
+    if (inputbox.value === '') {
+        alert("You must write something!");
+    } else {
+        let li = document.createElement("li");
+        li.textContent = inputbox.value;
+        listcontainer.appendChild(li);
+        let span = document.createElement("span");
+        span.textContent = "\u00d7";
+        li.appendChild(span);
+    }
+    inputbox.value = "";
+    savedata();
+}
+
+listcontainer.addEventListener("click", function(e) {
+    if (e.target.tagName === "LI") {
+        e.target.classList.toggle("checked");
+        savedata();
+    } else if (e.target.tagName === "SPAN") {
+        e.target.parentElement.remove();
+        savedata();
+    }
+}, false);
+
+function savedata(){
+    localStorage.setItem("data",listcontainer.innerHTML);
+}
+
+function showtask(){
+    listcontainer.innerHTML = localStorage.getItem("data");
+}
+showtask();
